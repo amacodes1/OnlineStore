ffce3edda5406f3658a107aa48c63c7dcfdb7a5c
mercyama <mercyezihe2@gmail.com>
2024-07-10 19:33:49 +0100

    made changes

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,16 +3,22 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
+import Cart from "./Cart";
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
+  const toggleCart = () => {
+    setIsCartOpen(!isCartOpen);
+  };
+
   return (
-    <div>
+    <div className="relative">
       <div className="w-6 h-6 absolute left-[291px] top-[58px]" />
       <div className="bg-white w-full h-[60px] md:h-[80px] px-4 md:px-28 py-2 md:py-6 absolute left-0 top-[32px] flex justify-between items-center">
         <div className="flex items-center">
@@ -85,23 +91,31 @@ export default function Navbar() {
 
         {/* Shopping Cart on large screen */}
         <div className="hidden md:flex items-center gap-1">
-          <Link href="/cart">
-            <div className="relative py-2 flex items-center gap-0.5">
-              <div className="relative w-5 h-5">
-                <Image
-                  src="/images/shopping-cart-02.png"
-                  height={20}
-                  width={20}
-                  alt="cart"
-                />
-                <span className="absolute top-[-10px] right-[-10px] bg-green500 text-green50 text-xs font-semibold rounded-xl py-1 px-2 w-6 h-5 ">
-                  5
-                </span>
-              </div>
+          <button
+            onClick={toggleCart}
+            className="relative py-2 flex items-center gap-0.5"
+          >
+            <div className="relative w-5 h-5">
+              <Image
+                src="/images/shopping-cart-02.png"
+                height={20}
+                width={20}
+                alt="cart"
+              />
+              <span className="absolute top-[-10px] right-[-10px] bg-green500 text-green50 text-xs font-semibold rounded-xl py-1 px-2 w-6 h-5">
+                5
+              </span>
             </div>
-          </Link>
+          </button>
         </div>
       </div>
+
+      {/* Cart dropdown for large screens */}
+      {isCartOpen && (
+        <div className="hidden md:block absolute right-0 top-[100px] mt-2 z-20 w-full lg:w-[400px] lg:shadow-lg">
+          <Cart />
+        </div>
+      )}
     </div>
   );
 }
